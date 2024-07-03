11c225b7aafa10ceb80941131ee1caf3125531ec
Scott VanRavenswaay <scottvr@gmail.com>
2024-07-03 04:47:31 -0500

    Update calc.js

--- web/js/calc.js
+++ web/js/calc.js
@@ -45,20 +45,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
         calculate();
     }
-function validateInput(variable, value) {
-    const ranges = {
-        R_star: [0, Infinity],
-        f_p: [0, 1],
-        n_e: [0, Infinity],
-        f_l: [0, 1],
-        f_i: [0, 1],
-        f_c: [0, 1],
-        L: [0, Infinity],
-        N: [0, Infinity]
-    };
-    const [min, max] = ranges[variable];
-    return value >= min && value <= max;
-}
+    
+    function validateInput(variable, value) {
+        const ranges = {
+            R_star: [0, Infinity],
+            f_p: [0, 1],
+            n_e: [0, Infinity],
+            f_l: [0, 1],
+            f_i: [0, 1],
+            f_c: [0, 1],
+            L: [0, Infinity],
+            N: [0, Infinity]
+        };
+        const [min, max] = ranges[variable];
+        return value >= min && value <= max;
+    }
 
     function calculate() {
         const values = {};
