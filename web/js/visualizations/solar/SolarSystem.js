59a30cec56c9f0d2b967eaf9e96fc42b83271477
ScottVR <scottvr@gmail.com>
2024-12-21 04:17:44 -0600

    snuff import

--- web/js/visualizations/solar/SolarSystem.js
+++ web/js/visualizations/solar/SolarSystem.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+//import React, { useEffect, useRef, useState } from 'react';
 import { select, forceSimulation, forceManyBody, forceCenter, forceCollide, forceRadial } from 'd3';
 
 const SolarSystemViz = ({ model, variables, onFactorHighlight }) => {
