c6adb18a735207e7d76d92e0e410e68a769d7cab
ScottVR <scottvr@gmail.com>
2024-12-21 03:58:41 -0600

    feh

--- web/js/visualizations/solar/SolarSystem.js
+++ web/js/visualizations/solar/SolarSystem.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+//import React, { useEffect, useRef, useState } from 'react';
 import { select, forceSimulation, forceManyBody, forceCenter, forceCollide, forceRadial } from 'd3';
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { Slider } from '@/components/ui/slider';
